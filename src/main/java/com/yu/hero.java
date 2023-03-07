d148bf1512f1b90bfc9e2beb7cd5f6c176e68d60
HeroYu <1551405455@qq.com>
2023-03-07 21:40:27 +0800

    first commit

---
+++ src/main/java/com/yu/hero.java
@@ -0,0 +1,13 @@
+package com.yu;
+
+/**
+ * @program: gitTest
+ * @description:
+ * @author: Yu
+ * @create: 2023-03-07 21:39
+ **/
+public class hero {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
